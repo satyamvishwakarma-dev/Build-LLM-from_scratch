170e2cd2df2c6441e0668c040233749aa27a922f
Satyam Vishwakarma <officialsatyamvishwakarma06@gmail.com>
2026-07-15 11:47:10 +0530

    completed the NN training

--- Learning_Pytorch/listing_a9.py
+++ Learning_Pytorch/listing_a9.py
@@ -17,19 +17,19 @@ num_epochs = 3
 for epoch in range(num_epochs):
     model.train()
 
-for batch_idx, (features, labels) in enumerate(train_loader):
-    logits = model(features)
+    for batch_idx, (features, labels) in enumerate(train_loader):
+        logits = model(features)
 
-    loss = F.cross_entropy(logits, labels)
+        loss = F.cross_entropy(logits, labels)
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-    # LOGGING
-    print(f"Epoch: {epoch+1:03d}/{num_epochs:03d}"
-          f" | Batch {batch_idx+1:03d}/{len(train_loader):03d}"
-          f" | Train Loss: {loss:.2f}")
+        # LOGGING
+        print(f"Epoch: {epoch+1:03d}/{num_epochs:03d}"
+              f" | Batch {batch_idx:03d}/{len(train_loader):03d}"
+              f" | Train Loss: {loss:.2f}")
 
 if __name__ == "__main__":
     model.eval()
