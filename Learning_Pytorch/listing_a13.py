613555c6636cde99f762c561439d2241d401e48d
Satyam Vishwakarma <officialsatyamvishwakarma06@gmail.com>
2026-07-16 18:00:52 +0530

    Implement distributed training setup and data loading in listing_a13.py

--- Learning_Pytorch/listing_a13.py
+++ Learning_Pytorch/listing_a13.py
@@ -1,5 +1,65 @@
 import torch
 from torch.utils.data.distributed import DistributedSampler
 from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.distributed import init_process_group
+from torch.distributed import init_process_group, destroy_process_group
+import torch.multiprocessing as mp
+import os
+from torch.utils.data import DataLoader
+from listing_a9 import compute_accuracy
+from listing_a6 import train_ds
+from listing_a7 import test_loader
+from listing_a4 import NeuralNetwork
+
+def ddp_setup(rank, world_size):
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12345'
+    init_process_group(
+        backend='nccl',
+        rank=rank,
+        world_size=world_size
+
+    )
+    torch.cuda.set_device(rank)
+
+def prepare_dataset():
+    # insert dataset preparation code
+    train_loader = DataLoader(
+        dataset=train_ds,
+        batch_size=2,
+        shuffle=False,
+        pin_memory=True,
+        drop_last=True,
+        sampler=DistributedSampler(train_ds)
+    )
+    return train_loader, test_loader
+
+def main(rank, world_size, num_epochs):
+    ddp_setup(rank, world_size)
+    train_loader, test_loader = prepare_dataset()
+    model = NeuralNetwork(num_input=2, num_output=2)
+    model.to(rank)
+    optimizer = torch.optim.SGD(model.parameters(), lr=0.5)
+    model = DDP(model, device_ids=[rank])
+    for epoch in range(num_epochs):
+        for features, labels in train_loader:
+            features, labels = features.to(rank), labels.to(rank)
+            # insert model prediction and backpropagation code
+            print(f"[GPU{rank}] Epoch: {epoch+1:03d}/{num_epochs:03d}"
+                  f" | Batchsize {labels.shape[0]:03d}"
+                  f" | Train/Val Loss: {loss:.2f}")
+                  
+    model.eval()
+    train_acc = compute_accuracy(model, train_loader, device=rank)
+    print(f"[GPU{rank}] Traning Accuracy", train_acc)
+    test_acc = compute_accuracy(model, test_loader, device=rank)
+    print(f"[GPU{rank}] Testing Accuracy", test_acc)
+    destroy_process_group()
+
+if __name__ == "__main__":
+    print("Number of GPUs Available: ", torch.cuda.device_count())
+    torch.manual_seed(123)
+    num_epochs = 3
+    world_size = torch.cuda.device_count()
+    mp.spawn(main, args=(world_size, num_epochs), nprocs=world_size)
+
 
