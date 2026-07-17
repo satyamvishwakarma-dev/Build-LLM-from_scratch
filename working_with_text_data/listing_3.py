7c00c685af2f3520bc577f9094ca1bb46cc147ca
Satyam Vishwakarma <officialsatyamvishwakarma06@gmail.com>
2026-07-17 16:39:01 +0530

    Fix execution count in listing_2.ipynb and update SimpleTokenizerV1 class for correct vocabulary mapping and decoding functionality

--- working_with_text_data/listing_3.py
+++ working_with_text_data/listing_3.py
@@ -4,7 +4,7 @@ import re
 class SimpleTokenizerV1:
     def __init__(self, vocab):
         self.str_to_int = vocab
-        self.int_to_str = {i:s for s,i in vocab.int()}
+        self.int_to_str = {i:s for s,i in vocab.items()}
 
     def encode(self, text):
         preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
@@ -15,3 +15,30 @@ class SimpleTokenizerV1:
         return ids
     
     def decode(self, ids):
+        text = " ".join(self.int_to_str[i] for i in ids)
+        text = re.sub(r'\s+([,.?!"()\'])', r'\1', text)
+        return text
+    
+if __name__ == "__main__":
+    # Opening the text file and preprocessing it
+    with open("E:\\Build_LLM\\working_with_text_data\\the-verdict.txt", "r", encoding="utf-8") as f:
+        raw_text = f.read()
+    preprocessed = re.split(r'([,.:;?_!"()\']|--|\s)', raw_text)
+    preprocessed = [item for item in preprocessed if item.strip()]
+
+    # Getting all the world in soeted list
+    all_words = sorted(set(preprocessed))
+
+    # Creating a vocabulary
+    vocab = {token:integer for integer, token in enumerate(all_words)}
+
+    # Creating a tokenizer
+    tokenizer = SimpleTokenizerV1(vocab)
+    text = """"It's the last he painted, you know,"
+            Mrs. Gisburn said with pardonable pride"""
+    
+    # Encoding the text
+    ids = tokenizer.encode(text)
+    print("Encoded ids:", ids)
+    # print(ids)
+    print("Encoded text:", tokenizer.decode(ids))
