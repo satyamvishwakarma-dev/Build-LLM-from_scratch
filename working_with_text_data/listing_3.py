82f678c2772532cf51390778c85752160f2e7b14
Satyam Vishwakarma <officialsatyamvishwakarma06@gmail.com>
2026-07-17 16:18:26 +0530

    Add SimpleTokenizerV1 class for text encoding and decoding

---
+++ working_with_text_data/listing_3.py
@@ -0,0 +1,17 @@
+import re
+
+
+class SimpleTokenizerV1:
+    def __init__(self, vocab):
+        self.str_to_int = vocab
+        self.int_to_str = {i:s for s,i in vocab.int()}
+
+    def encode(self, text):
+        preprocessed = re.split(r'([,.?_!"()\']|--|\s)', text)
+        preprocessed = [
+            item.strip() for item in preprocessed if item.strip()
+        ]
+        ids = [self.str_to_int[s] for s in preprocessed]
+        return ids
+    
+    def decode(self, ids):
