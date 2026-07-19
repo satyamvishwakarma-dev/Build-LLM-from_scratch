7eda36c83b33794acbc49a3600961861e99f8980
Satyam Vishwakarma <officialsatyamvishwakarma06@gmail.com>
2026-07-19 14:45:53 +0530

    Update GPTDataset class to include stride parameter for chunking input text

--- working_with_text_data/listing_5.py
+++ working_with_text_data/listing_5.py
@@ -2,8 +2,21 @@ import torch
 from torch.utils.data import Dataset, dataloader
 
 class GPTDataset(Dataset):
-    def __init__(self, txt, tokenizer, max_length):
+    def __init__(self, txt, tokenizer, max_length, stride):
         self.input_ids = []
         self.target_ids = []
 
         token_ids = tokenizer.encode(txt)
+
+        for i in range(0, len(token_ids) - max_length, stride):
+            input_chunk = token_ids[i + 1: i + max_length + 1]
+            target_chunk = token_ids[i + 1: i + max_length + 1]
+            self.input_ids.append(torch.tensor(input_chunk))
+            self.target_ids.append(torch.tensor(target_chunk))
+            
+        
+    def __len__(self):
+        return len(self.input_ids)
+    
+    def __getitem__(self, index):
+        return self.input_ids[index], self.target_ids[index]
