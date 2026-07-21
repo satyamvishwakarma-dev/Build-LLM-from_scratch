a18cd4a9bebdb9c4762a58456d3de753d5be1b0d
Satyam Vishwakarma <officialsatyamvishwakarma06@gmail.com>
2026-07-21 15:45:06 +0530

    Add topic_4.py for attention mechanism implementation

---
+++ chapter_3/topic_4.py
@@ -0,0 +1,22 @@
+from os import name
+
+import torch
+from topic_3 import inputs
+
+x_2 = inputs[1]
+d_in = inputs.shape[1]
+d_out = 2
+
+torch.manual_seed(123)
+
+W_query = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+W_key = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+W_value = torch.nn.Parameter(torch.rand(d_in, d_out), requires_grad=False)
+
+query_2 = x_2 @ W_query
+key_2 = x_2 @ W_key
+value_2 = x_2 @ W_value
+
+
+if __name__ == "__main__":
+    print(query_2)
