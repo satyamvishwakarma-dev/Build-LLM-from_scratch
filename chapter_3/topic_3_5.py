cd9d9a630d6dbec9c0271802c27f3fc301d35632
Satyam Vishwakarma <officialsatyamvishwakarma06@gmail.com>
2026-07-22 20:33:55 +0530

    Add attention weight calculations and masking in topic_3_5

--- chapter_3/topic_3_5.py
+++ chapter_3/topic_3_5.py
@@ -1,1 +1,31 @@
 import torch
+from listing_3_2 import sa_v2
+from topic_3_4 import d_out, d_in, inputs
+
+queries = sa_v2.W_query(inputs)
+keys = sa_v2.W_key(inputs)
+attn_scores = queries @ keys.T
+attn_weights = torch.softmax(
+    attn_scores / keys.shape[-1] ** 0.5, dim=-1
+)
+context_length = attn_scores.shape[0]
+mask_simple = torch.tril(torch.ones(context_length, context_length))
+
+masked_sample = attn_weights * mask_simple
+
+row_sums = masked_sample.sum(dim=-1, keepdim=True)
+masked_simple_norm = masked_sample / row_sums
+
+
+if __name__ == "__main__":
+    print("\n===ATTN WEIGHTS ===")
+    print(attn_weights)
+
+    print("\n===MASK SIMPLE ===")
+    print(mask_simple)
+
+    print("\n===MASKED SAMPLE ===")
+    print(masked_sample)
+
+    print("===ATTANTION WEIGHT MATRIX WITH SUM 1 ===")
+    print(masked_simple_norm)
