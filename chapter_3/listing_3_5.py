d63b510b6599c77f07f2f1ad7faf1ae85375af48
Satyam Vishwakarma <officialsatyamvishwakarma06@gmail.com>
2026-07-24 20:37:25 +0530

    feat: add context vector computation and example usage in MultiHeadAttention class

--- chapter_3/listing_3_5.py
+++ chapter_3/listing_3_5.py
@@ -1,3 +1,4 @@
+from sympy import sec
 import torch
 import torch.nn as nn
 
@@ -44,3 +45,34 @@ class MultiHeadAttention(nn.Module):
         context_vectors = context_vectors.contiguous().view(b, num_tokens, self.d_out)
         context_vectors = self.out_proj(context_vectors)
         return context_vectors
+
+
+a = torch.tensor(
+    [
+        [
+            [
+                [0.2745, 0.6584, 0.2775, 0.8573],
+                [0.8993, 0.0390, 0.9268, 0.7388],
+                [0.7179, 0.7058, 0.9156, 0.4340],
+            ],
+            [
+                [0.0772, 0.3565, 0.1479, 0.5331],
+                [0.4066, 0.2318, 0.4545, 0.9737],
+                [0.4606, 0.5159, 0.4220, 0.5786],
+            ],
+        ]
+    ]
+)
+
+first_head = a[0, 0, :, :]
+first_res = first_head @ first_head.T
+
+second_head = a[0, 1, :, :]
+second_res = second_head @ second_head.T
+
+if __name__ == "__main__":
+    print(a @ a.transpose(2, 3))
+
+    print("First Head:\n", first_res)
+
+    print("\nSecond Head:\n", second_res)
