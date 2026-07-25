ff4cf310b4bbe87fc7cba94fa2d5ab51fbcd85f1
Satyam Vishwakarma <officialsatyamvishwakarma06@gmail.com>
2026-07-25 14:39:56 +0530

    feat: add GPT configuration for 124M model in topic_4_1.py

---
+++ chapter_4/topic_4_1.py
@@ -0,0 +1,9 @@
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,  # Vocabulary Size
+    "context_length": 1024,  # Context Length
+    "emb_dim": 768,  # Embedding Dimension
+    "n_layer": 12,  # Number of Layers
+    "n_head": 12,  # Number of Attention Heads
+    "drop_rate": 0.1,  # Dropout Rate
+    "qkv_bias": False,  # Query Key Value Bias
+}
