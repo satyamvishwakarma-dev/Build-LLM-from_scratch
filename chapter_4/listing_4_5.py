e2a3b7aa81d737ce9614a5a7b179b6e01baafd14
Satyam Vishwakarma <officialsatyamvishwakarma06@gmail.com>
2026-07-26 21:00:38 +0530

    feat: implement ExampleDeepNeuralNetwork class with gradient printing functionality

--- chapter_4/listing_4_5.py
+++ chapter_4/listing_4_5.py
@@ -1,4 +1,47 @@
+from requests.packages import target
 import torch
 from torch import nn
 from topic_4_1 import GPT_CONFIG_124M
 
+class ExampleDeepNeuralNetwork(nn.Module):
+    def __init__(self, layer_sizes, use_shortcut):
+        super().__init__()
+        self.use_shortcut = use_shortcut
+        self.layers = nn.ModuleList([
+            nn.Sequential(nn.Linear(layer_sizes[0], layer_sizes[1]), nn.GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[1], layer_sizes[2]), nn.GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[2], layer_sizes[3]), nn.GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[3], layer_sizes[4]), nn.GELU()),
+            nn.Sequential(nn.Linear(layer_sizes[4], layer_sizes[5]), nn.GELU()),
+        ])
+
+    def forward(self, x):
+        for layer in self.layers:
+            layer_output = layer(x)
+            if self.use_shortcut and x.shape == layer_output.shape:
+                x = x + layer_output
+            else:
+                x = layer_output
+        return x
+
+layer_sizes = [3,3,3,3,3,1]
+sample_input = torch.tensor([[1., 0., -1.]])
+torch.manual_seed(123)
+model_with_shortcut = ExampleDeepNeuralNetwork(layer_sizes, use_shortcut=False)
+
+def print_gradient(model, x):
+    output = model(x)
+    target = torch.tensor([[0.]])
+
+    loss = nn.MSELoss()
+    loss = loss(output, target)
+
+    loss.backward()
+
+    for name, param in model.named_parameters():
+        if 'weight' in name:
+            print(f"{name} has gradient mean of {param.grad.abs().mean().item()}")
+
+
+if __name__ == '__main__':
+    print_gradient(model_with_shortcut, sample_input)
