333e23b9588c98c10e503b7eb9272cc8febad766
Satyam Vishwakarma <officialsatyamvishwakarma06@gmail.com>
2026-07-27 19:55:10 +0530

    feat: rename text generation function to generate_text_simple and update usage

--- chapter_4/listing_4_8.py
+++ chapter_4/listing_4_8.py
@@ -1,7 +1,10 @@
 import torch
 from torch import nn
+import tiktoken
+from listing_4_7 import GPT_CONFIG_124M, model
 
-def generate_text(model, idx, max_new_tokens, context_size):
+
+def generate_text_simple(model, idx, max_new_tokens, context_size):
     for _ in range(max_new_tokens):
         idx_cond = idx[:, -context_size:]
         with torch.no_grad():
@@ -13,3 +16,26 @@ def generate_text(model, idx, max_new_tokens, context_size):
 
     return idx
 
+
+tokenizer = tiktoken.get_encoding("gpt2")
+
+start_context = "Hello, I am"
+encoded = tokenizer.encode(start_context)
+encoded_tensor = torch.tensor(encoded).unsqueeze(0)
+
+model.eval()
+out = generate_text_simple(
+    model=model,
+    idx=encoded_tensor,
+    max_new_tokens=6,
+    context_size=GPT_CONFIG_124M["context_length"],
+)
+
+decoded_text = tokenizer.decode(out.squeeze(0).tolist())
+if __name__ == "__main__":
+    print("\nEncoded:", encoded)
+    print("\nEncoded Tensor:", encoded_tensor)
+    print("\nEncoded Tensor Shape:", encoded_tensor.shape)
+    print("\nOutput:", out)
+    print("\nOutput Length:", len(out[0]))
+    print("\nDecoded Text:", decoded_text)
