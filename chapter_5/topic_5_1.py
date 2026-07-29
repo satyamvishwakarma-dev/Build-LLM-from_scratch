77344433b7cd83b7fb27f8594f77d26739b29f20
Satyam Vishwakarma <officialsatyamvishwakarma06@gmail.com>
2026-07-29 13:47:16 +0530

    feat: add initial implementation of GPT model configuration and evaluation

---
+++ chapter_5/topic_5_1.py
@@ -0,0 +1,16 @@
+import torch
+from chapter_4.listing_4_7 import GPTModel
+
+GPT_CONFIG_124M = {
+    "vocab_size": 50257,
+    "context_length": 256,
+    "emb_dim": 768,
+    "n_heads": 12,
+    "n_layers": 12,
+    "drop_rate": 0.1,
+    "qkv_bias": False
+}
+
+torch.manual_seed(123)
+model = GPTModel(GPT_CONFIG_124M)
+model.eval()
