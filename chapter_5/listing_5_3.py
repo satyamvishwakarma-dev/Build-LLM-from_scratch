078f47930481d1de08ecefc8856e222953c870ce
Satyam Vishwakarma <officialsatyamvishwakarma06@gmail.com>
2026-07-30 11:39:25 +0530

    feat: implement training and evaluation functions in listing_5_3.py for model training workflow

---
+++ chapter_5/listing_5_3.py
@@ -0,0 +1,60 @@
+import torch
+from chapter_4.listing_4_8 import generate_text_simple
+from chapter_5.listing_5_1 import calc_loss_batch, text_to_token_ids, token_ids_to_text
+from chapter_5.listing_5_2 import calc_loss_loader
+
+def train_model_simple(
+        model, train_loader, val_loader, optimizer, device, num_epochs, eval_freq, eval_iter, start_context, tokenizer
+        ):
+    train_losses, val_losses, track_tokens_seen = [], [], []
+    token_seen, global_step = 0, -1
+
+    for epoch in range(num_epochs):
+        model.train()
+        for input_batch, target_batch in train_loader:
+            optimizer.zero_grad()
+            loss = calc_loss_batch(
+                input_batch, target_batch, model, device
+            )
+            loss.backward()
+            optimizer.step()
+            token_seen += input_batch.numel()
+            global_step += 1
+
+            if global_step % eval_freq == 0:
+                train_loss, val_loss = evaluate_model(
+                    model, train_loader, val_loader, device, eval_iter
+                )
+                train_losses.append(train_loss)
+                val_losses.append(val_loss)
+                track_tokens_seen.append(token_seen)
+                print(f"Ep {epoch+1} (Step {global_step:06d}): Train Loss {train_loss:.3f}, Val Loss {val_loss:.3f}")
+
+        generate_and_print_sample(
+            model, tokenizer, device, start_context
+        )
+    return train_losses, val_losses, track_tokens_seen
+
+def evaluate_model(model, train_loader, val_loader, device, eval_iter):
+    model.eval()
+    with torch.no_grad():
+        train_loss = calc_loss_loader(train_loader, model, device, num_batches=eval_iter)
+        val_loss = calc_loss_loader(val_loader, model, device, num_batches=eval_iter)
+    model.train()
+    return train_loss, val_loss
+
+def generate_and_print_sample(model, tokenizer, device, start_context):
+    model.eval()
+    contexr_size = model.positional_embedding.weight.shape[0]
+    encoded = text_to_token_ids(start_context, tokenizer).to(device)
+    with torch.no_grad():
+        token_ids = generate_text_simple(
+            model = model,
+            idx = encoded,
+            max_new_tokens = 50,
+            context_size = contexr_size,
+        )
+    decoded_text = token_ids_to_text(token_ids, tokenizer)
+    print(decoded_text.replace("\n", ""))
+    model.train()
+    
