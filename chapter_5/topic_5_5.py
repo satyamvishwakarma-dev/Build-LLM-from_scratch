2c9048e61a28d1f1e664d4c7fc1c03e9007e4285
Satyam Vishwakarma <officialsatyamvishwakarma06@gmail.com>
2026-07-31 11:29:14 +0530

    Update chapter 5 code without heavy model weights

--- chapter_5/topic_5_5.py
+++ chapter_5/topic_5_5.py
@@ -1,5 +1,12 @@
 import urllib.request
 
-url = "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch05/01_main-chapter-code/gpt_download.py"
-filename = url.split("/")[-1]
-urllib.request.urlretrieve(url, filename)
+from chapter_5.gpt_download import download_and_load_gpt2
+
+# url = "https://raw.githubusercontent.com/rasbt/LLMs-from-scratch/main/ch05/01_main-chapter-code/gpt_download.py"
+# filename = url.split("/")[-1]
+# urllib.request.urlretrieve(url, filename)
+
+
+settings, params = download_and_load_gpt2(
+    model_size="124M", models_dir="gpt2"
+)
